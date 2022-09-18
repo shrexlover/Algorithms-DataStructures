5343d424d758fef721b387b018cda6228b7d9bdb
student_piwo <97889894+shrexlover@users.noreply.github.com>
2022-09-18 21:04:32 +0200

    Add files via upload

---
+++ lab 3 Graph Algorithms/graph_gen.py
@@ -0,0 +1,14 @@
+import random
+n = 100
+num_of_lines = int(((n*(n-1))/2) * 0.6)
+def linegen(n, num_of_lines):
+    for i in range(num_of_lines):
+        a = str(random.randint(1, n))
+        b = str(random.randint(1, n))
+        c = a + " " + b + "\n"
+        c.join(" ")
+        f.write(c)
+
+f = open("graph", "w")
+linegen(n, num_of_lines)
+f.close()
